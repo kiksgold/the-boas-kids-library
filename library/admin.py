918ece8d76d3454607594460235ae4ab8a5cd737
kiksgold <kiksgold@gmail.com>
2022-11-20 20:47:40 +0000

    Add Book Model to Admin

--- library/admin.py
+++ library/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Book
 from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'author', 'slug', 'uploaded_on')
+    search_fields = ['title', 'summary']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', 'author', 'uploaded_on')
