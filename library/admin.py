8ec0dc318cd537ca8a34c4d95f8ff1d4a87e3fc8
kiksgold <kiksgold@gmail.com>
2022-11-20 21:02:51 +0000

    Add Admin Object for BookInstance Model

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book, Review
+from .models import Book, Review, BookInstance
 from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Book)
 class BookAdmin(SummernoteModelAdmin):
@@ -13,7 +14,22 @@ class BookAdmin(SummernoteModelAdmin):
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    #Admin Object for Review Model
+    # Admin Object for Review Model
     list_display = ('name', 'body', 'book', 'uploaded_on')
     list_filter = ('book', 'uploaded_on')
     search_fields = ('name', 'email', 'body')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    # Admin Object for Book Instance Model
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'id', 'status', 'borrower', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
